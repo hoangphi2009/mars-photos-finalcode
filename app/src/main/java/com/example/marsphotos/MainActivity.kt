8110923c601a4f4f01b82cfcc6b1422de1fdf8da
Hoang Phi <phido76792@gmail.com>
2023-12-04 18:12:17 +0700

    push code

---
+++ app/src/main/java/com/example/marsphotos/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.marsphotos
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
+import com.example.marsphotos.ui.MarsPhotosApp
+import com.example.marsphotos.ui.theme.MarsPhotosTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        setContent {
+            MarsPhotosTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MarsPhotosApp()
+                }
+            }
+        }
+    }
+}
